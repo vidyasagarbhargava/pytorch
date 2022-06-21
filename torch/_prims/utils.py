e8da9b11ae2543b244e0453ffcf8e6736ac6486b
lezcano <lezcano-93@hotmail.com>
2022-06-21 15:52:00 +0000

    Add prim.svd, refs.linalg.svd, and refs.linalg.svdvals
    This is the first prim / ref added that has multiple returns.
    There is an issue with `out_wrapper_multi` as currently implemented
    (left a note). It assumes that the API is `svd(X, U=U, S=S, Vh=Vh)`,
    when it's actually `svd(X, out=(U, S, Vh))`.
    
    Even more, if we want to model PyTorch exactly, it should return a
    `torch.return_types.svd`, rather than a `Tuple`.
    
    There is an issue with
    As per title
    
    ghstack-source-id: 3652fe6a2b27958111e265019810f029153fdf0d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78616

--- torch/_prims/utils.py
+++ torch/_prims/utils.py
@@ -1083,14 +1083,23 @@ def make_contiguous_strides_for(shape: ShapeType) -> Tuple[int, ...]:
     multiplier = 1
     strides = []
     for l in reversed(shape):
+        strides.append(multiplier)
         if l != 0:
-            strides.append(multiplier)
-            multiplier = l * multiplier
-        else:
-            strides.append(multiplier)
+            multiplier *= l
 
     result = tuple(reversed(strides))
     return result
+
+
+def make_batched_column_major_strides_for(shape: ShapeType) -> Tuple[int, ...]:
+    """
+    Returns the strides of a batch of contiguous column-major matrices
+    This is often used when calling external libraries like BLAS/LAPACK/cuSolver...
+    """
+    contiguous_strides = make_contiguous_strides_for(shape)
+    if len(shape) < 2:
+        return contiguous_strides
+    return contiguous_strides[:-2] + (1, max(shape[-2], 1))
 
 
 def compute_reduction_output_shape(
